986d2f627d81e5dc60f55674d566aa92b10d8df6
Amit Yadav <amityadav010213@gmail.com>
2026-08-21 12:03:26 +0530

    error solved

--- Array/ContainsDuplicate.java
+++ Array/ContainsDuplicate.java
@@ -1,4 +1,6 @@
-class Solution {
+import java.util.Arrays;
+
+class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
