23ac306f95f3e7f09ecaee49a601dc9adfd201d3
Amit Yadav <amityadav010213@gmail.com>
2026-09-02 21:58:22 +0530

    set, set size, set elements, set remove

--- Hashing.java
+++ Hashing.java
@@ -4,10 +4,18 @@ public class  Hashing {
     public static void main (String args[]) {
         HashSet <Integer> set = new HashSet<>();
 
+        // Add
         set.add(1);
         set.add(2);
+        set.add(3);
+        set.add(4);
         set.add(2);
 
+        System.out.println("Size is " + set.size());
+
+        System.out.println("Print all elements" + set);
+
+        // Search
         if(set.contains(1)) {
             System.out.println("Contains ");
         }
@@ -15,5 +23,17 @@ public class  Hashing {
         if(!set.contains(6)) {
             System.out.println("Does not Contains ");
         }
+
+        //Delete
+        set.remove(1);
+        if(!set.contains(1)) {
+            System.out.println("Does not Contains 1 ");
+        }
+
+        // Special Iterator because we cannot use for loop, there is no index of set elements. 
+
+        
+
+
     }
 }
