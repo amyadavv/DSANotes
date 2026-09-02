9ca4a0eb99426b96ec48a9ef94d61d425c488db2
Amit Yadav <amityadav010213@gmail.com>
2026-09-02 18:20:43 +0530

    added one more approach

--- Array/ContainsDuplicate.java
+++ Array/ContainsDuplicate.java
@@ -11,3 +11,31 @@ class ContainsDuplicate {
         return false;
     }
 }
+
+/*
+
+Without sort
+
+
+public class testing {
+    public static boolean duplicateElement(int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[i]==arr[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 5 };
+        System.out.println(duplicateElement(arr));
+    }
+}
+
+
+*/
